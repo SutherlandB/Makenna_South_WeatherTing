5c9ff743c4f93f69e3afd6ba88beb184125dceb2
South <thesudman100@gmail.com>
2021-08-01 00:42:28 -0400

    location info implemented

--- scripts.js
+++ scripts.js
@@ -1,12 +1,15 @@
 //global variable
-let userLat = 0;
-let userLong = 0;
+var userLat = 0;
+var userLong = 0;
 function successCallback(pos){
     console.log(pos);
     
     userLat = pos.coords.latitude;
     userLong = pos.coords.longitude;
+    toString(userLat);
+    toString(userLong);
     console.log("lat: " + userLat + " long: " + userLong);
+    fetch('http://api.weatherapi.com/v1/forecast.json?key=343026a1d40b4be1acd15649210108&q=' + userLat + ',' + userLong + '&days=3&aqi=no&alerts=no').then(response => response.json()).then(data => console.log(data));
 }
 const errorCallback = (error) => {
     console.log(error);
